2677027498a03bb308200c8cfb90a71ffe9f7f52
Kooooddiiii <chikkychime@gmail.com>
2022-11-20 16:05:25 +0100

    Updated SCSS and fixed Mobile nav bug

--- dist/scripts/nav.js
+++ dist/scripts/nav.js
@@ -2,21 +2,12 @@ const navbar = document.querySelector('#nav')
 
 window.addEventListener('scroll', function(e) {
   const lastPosition = window.scrollY
-  if (lastPosition > 600 ) {
+  if (lastPosition > 100 ) {
     navbar.classList.add('active')
-  } else if (navbar.classList.contains('active')) {
-    navbar.classList.remove('active')
-  } else {
-    navbar.classList.remove('active')
   }
-})
-window.addEventListener('scroll', function(e) {
-  const lastPosition = window.scrollY
-  if (lastPosition > 50 ) {
-    navbar.classList.add('pre-active')
-  } else if (navbar.classList.contains('pre-active')) {
-    navbar.classList.remove('pre-active')
+  else if (navbar.classList.contains('active')) {
+    navbar.classList.remove('active')
   } else {
-    navbar.classList.remove('pre-active')
+    navbar.classList.remove('active')
   }
 })
